54d4aa30c528feef4bc082f21d2eff1dedd3b094
swaroop <swaroopiit@gmail.com>
2024-11-07 19:16:55 +0530

    Added some more content

--- 01_flask_server/server.py
+++ 01_flask_server/server.py
@@ -2,8 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def prices():
+    plant_prices = {
+                    "rose" : 100,
+                    "orchid" : 200,
+                    "marigold" : 300
+                }
+    data = ""
+    for plant, price in plant_prices.items():
+        data += f"{plant} : {price} <br/>"
+    return data
+
 @app.route('/')
 def home():
-    return "Hai, this is home page!"
+    return prices()
 
 app.run()
